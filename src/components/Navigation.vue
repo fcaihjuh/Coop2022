b13424976e9db65b60b85aa222509997d690fb31
fcaihjuh <75486182+fcaihjuh@users.noreply.github.com>
2022-02-16 14:26:54 +0100

    Update Navigation.vue

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,29 +1,47 @@
 <template>
+
 <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-        <router-link class="navbar-item" to="/">Coop</router-link>
-    </div>
+  <div class="navbar-brand">
+    <router-link class="navbar-item" to="/">Coop</router-link>
+  </div>
+
+ 
     <div class="navbar-start">
-        <router-link class="navbar-item" to="/">Conversations</router-link>
-        <router-link class="navbar-item" to="/creer-conversation">Créer une conversation</router-link>
+      <router-link class="navbar-item" to="/">Conversations</router-link>
+      <router-link class="navbar-item" to="creer-conversation">Creer une conversation</router-link>
+      <router-link class="navbar-item" to="/membres">Tous les Membres</router-link>
     </div>
+
     <div class="navbar-end">
-        <div class="navbar-item">
-            <div>
-                Connecté en tant que 
-                <b>{{$store.state.member.fullname}}</b>
-            </div>
+      <div class="navbar-item">
+        <div>
+          Connecté en tant que <b>{{$store.state.member.fullname}}</b>
+        </div> 
+      </div>
+      <div class="navbar-item">
+        <div class="buttons">
+          <router-link class="button is-warning" to="/deconnection">se deconnecter</router-link>
         </div>
-        <div class="navbar-item">
-            <div class="buttons">
-                <router-link class="button is-warning" to="deconnexion">Se déconnecter</router-link>
-                </div>
-        </div>
+      </div>
+     
+
     </div>
+  
 </nav>
+
 </template>
+
 <script>
-export default{}
+export default{
+    data(){
+        return{
+            conversations:[]
+        }
+    },
+    mounted(){
+        this.$api.get('channels').then(response=>{
+            this.conversations = response.data
+        })
+    }
+}
 </script>
-<style>
-</style>
